39c2dfb02378d14b972a9309993f50bcd1ecd2a5
Thomas Barros Camara <thomasbcamara@gmail.com>
2022-03-16 17:54:55 +0100

    Remove other squads code.

--- deployments/s5/main.tf
+++
@@ -1,9 +0,0 @@
-module "s5" {
-  source = "../../modules/s5"
-
-  region = var.region
-  env_name = var.env_name
-  domain_name = var.domain_name
-  fe_subdomain = var.fe_subdomain
-}
-
--- modules/s5/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "vpc_id" { 
-    description = "The VPC ID"
-    value = module.vpc.vpc_id
-}
